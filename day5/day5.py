ed97ec0adfd2b031cff8833a5c0c09e022236e3d
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-12-07 07:09:41 -0800

    "simplify" intcode computer

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,6 @@
-with open('input.txt') as f:
-    onums = [int(num) for num in f.read().split(',')]
+import operator
+
+onums = [int(num) for num in open('input.txt').read().strip().split(',')]
 
 jumps = {1: 4, 
         2: 4, 
@@ -10,54 +11,37 @@ jumps = {1: 4,
         7: 4, 
         8: 4}
 
+addmul = lambda op : operator.add if op == 1 else operator.mul
+jumpif = lambda op : operator.ne if op == 5 else operator.eq
+trufal = lambda op : operator.lt if op == 7 else operator.eq
+
 def test(innum: int):
     nums = onums[:]
     i = 0
-    while i < len(nums) and nums[i] != 99:
-        opcode = nums[i]
+    while i < len(nums):
+        opcode = nums[i] % 100
         args = []
-        if len(str(nums[i])) >= 3:
-            opnum = nums[i]
-            opcode = opnum % 10
-            modes = opnum // 10 // 10
-            argc = 0
-            while argc < jumps[opcode]-1:
-                if modes % 10 == 1:
-                    args.append(nums[i+argc+1])
-                else:
-                    args.append(nums[nums[i+argc+1]])
-                modes = modes // 10
-                argc += 1
-        else:
-            argc = 1
-            while argc < jumps[opcode]:
-                args.append(nums[nums[i+argc]])
-                argc += 1
-        if opcode == 1:
-            nums[nums[i+3]] = args[0] + args[1]
-        elif opcode == 2:
-            nums[nums[i+3]] = args[0] * args[1]
+        modes = nums[i] // 10 // 10
+        argc = 0
+        while argc < jumps[opcode]-1:
+            if modes % 10 == 1:
+                args.append(nums[i+argc+1])
+            else:
+                args.append(nums[nums[i+argc+1]])
+            modes //= 10
+            argc += 1
+        if opcode in (1,2):
+            nums[nums[i+3]] = addmul(opcode)(args[0],args[1])
         elif opcode == 3:
             nums[nums[i+1]] = innum
         elif opcode == 4:
             if args[0] != 0:
                 return args[0]
-        elif opcode == 5 and args[0] != 0:
+        elif opcode in (5,6) and jumpif(opcode)(args[0], 0):
             i = args[1]
             continue
-        elif opcode == 6 and args[0] == 0:
-            i = args[1]
-            continue
-        elif opcode == 7:
-            if args[0] < args[1]:
-                nums[nums[i+3]] = 1
-            else:
-                nums[nums[i+3]] = 0
-        elif opcode == 8:
-            if args[0] == args[1]:
-                nums[nums[i+3]] = 1
-            else:
-                nums[nums[i+3]] = 0
+        elif opcode in (7,8):
+            nums[nums[i+3]] = int(trufal(opcode)(args[0],args[1]))
         elif opcode == 99:
             break
         i += jumps[opcode]
